71b2ac71f844c06d09a3b44cabbaa4bd92a38fb6
stefanDjacic <stefandjacic@gmail.com>
2021-04-14 22:57:39 +0200

    Follow logic added

---
+++ YumApp/Hubs/NotifyHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YumApp.Hubs
+{
+    public class NotifyHub : Hub
+    {
+        public async Task NotifyUser()
+        {
+            IGroupManager group = n
+            
+            await this.Clients.User("2").SendAsync()
+        }
+    }
+}
